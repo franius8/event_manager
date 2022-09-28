bac145329bc1d71d891866275724b7b2fb8722d8
franius8 <skawinskifranciszek@gmail.com>
2022-09-28 14:15:56 +0200

    Finish method determining best weekdays

--- lib/time_targeting.rb
+++ lib/time_targeting.rb
@@ -69,12 +69,41 @@ class BestWeekdayChecker
     @filename = filename
     @contents = define_contents
     @registration_weekdays = []
+    @weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    @highest_number_registered = 0
+    @best_weekdays = []
+    
+    perform_check
+  end
+
+  def perform_check
     convert_dates
+    print_weekdays
+    check_best_days
   end
 
   def convert_dates
     @contents.each { |row| @registration_weekdays << convert_to_date(row).strftime('%A') }
-    puts @registration_weekdays.tally
+  end
+
+  def print_weekdays
+    @weekdays.each do |day|
+      puts "#{day}: \t#{@registration_weekdays.count(day)} users registered."
+    end
+  end
+
+  def check_best_days
+    @weekdays.each do |day|
+      number = @registration_weekdays.count(day)
+      if @highest_number_registered < number
+        @highest_number_registered = number
+        @best_weekdays = []
+        @best_weekdays << day
+      elsif @highest_number_registered == number
+        @best_weekdays << day
+      end
+    end
+    print "The best weekdays for advertising are #{@best_weekdays.join(', ')}"
   end
 end
 
